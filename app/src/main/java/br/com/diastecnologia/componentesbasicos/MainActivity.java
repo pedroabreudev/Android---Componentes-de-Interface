efe2e810a3a344308119e2ebb3fd0e43eacfab42
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-06-09 09:21:14 -0400

    Caixa de Seleção - CheckBox

--- app/src/main/java/br/com/diastecnologia/componentesbasicos/MainActivity.java
+++ app/src/main/java/br/com/diastecnologia/componentesbasicos/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText campoEmail;
     private TextView textoResultado;
 
+    private CheckBox checkVerde;
+    private CheckBox checkBranco;
+    private CheckBox checkVermelho;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +27,40 @@ public class MainActivity extends AppCompatActivity {
         campoNome = findViewById(R.id.editNome);
         campoEmail = findViewById(R.id.editEmail);
         textoResultado = findViewById(R.id.textResultado);
+
+        //Checkbox
+        checkVerde = findViewById(R.id.checkVerde);
+        checkBranco = findViewById(R.id.checkBranco);
+        checkVermelho = findViewById(R.id.checkVermelho);
+    }
+    public void checkbox(){
+        String texto = "";
+
+        if(checkVerde.isChecked()){
+            texto = "Verde selecionado - ";
+        }
+        if(checkBranco.isChecked()){
+            texto = texto + "Branco selecionado - ";
+        }
+        if(checkVermelho.isChecked()){
+            texto = texto + "Vermelho selecionado - ";
+        }
+        textoResultado.setText(texto);
+
+
     }
     public void enviar(View view){
 
+        checkbox();
+        /*
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
 
         textoResultado.setText("Nome: " + nome + " Email: " + email );
+
+         */
+
+
 
     }
     public void limpar(View view){
