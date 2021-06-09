cdd06d06ba07737328fe86056c8e8146e0b46c56
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-06-09 08:57:22 -0400

    Criando Caixa de Texto e recuperando Valores de caixa de texto

---
+++ app/src/main/java/br/com/diastecnologia/componentesbasicos/MainActivity.java
@@ -0,0 +1,40 @@
+package br.com.diastecnologia.componentesbasicos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText campoNome;
+    private TextInputEditText campoEmail;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        campoNome = findViewById(R.id.editNome);
+        campoEmail = findViewById(R.id.editEmail);
+        textoResultado = findViewById(R.id.textResultado);
+    }
+    public void enviar(View view){
+
+        String nome = campoNome.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        textoResultado.setText("Nome: " + nome + " Email: " + email );
+
+    }
+    public void limpar(View view){
+        campoNome.setText("");
+        campoEmail.setText("");
+        textoResultado.setText("resultado");
+
+    }
+
+}
